5073a3108dc72497c91971ce0c0f8d83336b6a84
Parth Vohra <72933441+parthvohra25@users.noreply.github.com>
2022-12-22 16:43:55 +0530

    Create Lab_01.r

---
+++ Lab_01/Lab_01.r
@@ -0,0 +1,29 @@
+A <- 23.4
+B <- 45
+C <- 678
+print(A)
+print(B)
+print(C)
+ls()
+rm(C)
+ls()
+#I am learning R
+firstname = "Parth"
+lastname = "Vohra"
+print(firstname)
+print(lastname)
+C <- 678
+print(A+B+C)
+print(A*B*C)
+print((C/B)/A)
+print(C-B-A)
+print(exp(2))
+print(log10(10))
+print(log(10))
+print(log2(2))
+print(pi)
+print(sqrt(100))
+print(23+(4.5*2.3)/10)
+print((456/12)-log(90))
+print(exp(5)+(12/(5^6)))
+print(sqrt(45)*(12/3))
